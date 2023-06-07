69e964ba904945ce0b7b03e60d3bfb74dfe480a8
marcoweb <marcocarvalho.web@gmail.com>
2023-06-07 17:09:11 +0000

    Update (Genero)

--- app/src/main/java/application/controller/GeneroController.java
+++ app/src/main/java/application/controller/GeneroController.java
@@ -1,5 +1,7 @@
 package application.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -34,6 +36,34 @@ public class GeneroController {
 
         generoRepo.save(genero);
 
+        return "redirect:/genero/list";
+    }
+
+    @RequestMapping("/update")
+    public String update(Model model, @RequestParam("id") int id) {
+        Optional<Genero> genero = generoRepo.findById(id);
+
+        if(genero.isPresent()) {
+            model.addAttribute("genero", genero.get());
+            return "/genero/update";
+        }
+
+        return "redirect:/genero/list";
+    }
+
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    public String update(
+        @RequestParam("id") int id,
+        @RequestParam("nome") String nome
+    ) {
+        Optional<Genero> genero = generoRepo.findById(id);
+
+        if(genero.isPresent()) {
+            genero.get().setNome(nome);
+
+            generoRepo.save(genero.get());
+        }
+        
         return "redirect:/genero/list";
     }
 }
