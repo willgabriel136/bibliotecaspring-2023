e14836a90cfa2680ed831bc7238eec3422f21c2a
marcoweb <marcocarvalho.web@gmail.com>
2023-06-07 16:47:17 +0000

    Insert (Genero)

--- app/src/main/java/application/controller/GeneroController.java
+++ app/src/main/java/application/controller/GeneroController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import application.model.Genero;
 import application.model.GeneroRepository;
 
 @Controller
@@ -22,5 +25,15 @@ public class GeneroController {
     @RequestMapping("/insert")
     public String insert() {
         return "/genero/insert";
+    }
+
+    @RequestMapping(value = "/insert", method = RequestMethod.POST)
+    public String insert(@RequestParam("nome") String nome) {
+        Genero genero = new Genero();
+        genero.setNome(nome);
+
+        generoRepo.save(genero);
+
+        return "redirect:/genero/list";
     }
 }
